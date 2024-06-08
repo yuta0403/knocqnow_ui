351b6198536ff8c214603f93b6e73a5b59f9bd3a
yuta0403 <kanbenoyuta@gmail.com>
2024-06-08 15:08:09 +0900

    homeを仮で入れた

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,191 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        backgroundColor: Color(0xFFE0E5EC),
+        appBar: AppBar(
+          backgroundColor: Color(0xFFE0E5EC),
+          title: Text(
+            '友達',
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.black),
+              onPressed: () {},
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IconButton(
+              icon: Icon(Icons.more_vert, color: Colors.black),
+              onPressed: () {},
+            ),
+          ],
+          elevation: 0,
+        ),
+        body: ChatScreen(),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFFE0E5EC),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: NeumorphicIcon(icon: Icons.message),
+              label: 'Chats',
+            ),
+            BottomNavigationBarItem(
+              icon: NeumorphicIcon(icon: Icons.group),
+              label: 'Friends',
+            ),
+            BottomNavigationBarItem(
+              icon: NeumorphicIcon(icon: Icons.timeline),
+              label: 'Timeline',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+class NeumorphicIcon extends StatelessWidget {
+  final IconData icon;
+
+  const NeumorphicIcon({required this.icon}); // Added const here
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Color(0xFFE0E5EC),
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(-2, -2),
+            blurRadius: 5,
+          ),
+          BoxShadow(
+            color: Color(0xFFA3B1C6),
+            offset: Offset(2, 2),
+            blurRadius: 5,
+          ),
+        ],
       ),
+      child: Icon(icon, color: Colors.black),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10, // 仮のアイテム数
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: NeumorphicContainer(
+            child: ListTile(
+              leading: NeumorphicAvatar(),
+              title: Text('友達 $index'),
+              subtitle: Text('最後のメッセージ内容'),
+              trailing: Text('12:34'),
+              onTap: () {
+                // チャット画面に遷移する機能をここに追加
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class NeumorphicContainer extends StatelessWidget {
+  final Widget child;
+
+  NeumorphicContainer({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFE0E5EC),
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(-5, -5),
+            blurRadius: 15,
+          ),
+          BoxShadow(
+            color: Color(0xFFA3B1C6),
+            offset: Offset(5, 5),
+            blurRadius: 15,
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
+
+class NeumorphicIconButton extends StatelessWidget {
+  late final IconData iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    IconData? icon;
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Color(0xFFE0E5EC),
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(-2, -2),
+            blurRadius: 5,
+          ),
+          BoxShadow(
+            color: Color(0xFFA3B1C6),
+            offset: Offset(2, 2),
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Icon(icon, color: Colors.black),
+    );
+  }
+}
+
+class NeumorphicAvatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: Color(0xFFE0E5EC),
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(-2, -2),
+            blurRadius: 5,
+          ),
+          BoxShadow(
+            color: Color(0xFFA3B1C6),
+            offset: Offset(2, 2),
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Icon(Icons.person, color: Colors.black),
     );
   }
 }
